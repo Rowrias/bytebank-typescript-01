52d44f677d68e65befea1f6674fd3a711abeab35
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2024-02-06 13:50:07 -0300

    1.1 Requisitos em Javascript

---
+++ js/bytebank.js
@@ -0,0 +1,41 @@
+let saldo = 3000;
+
+const elementoSaldo = document.querySelector(".saldo-valor .valor");
+elementoSaldo.textContent = saldo;
+
+const elementoFormulario = document.querySelector(".block-nova-transacao form");
+elementoFormulario.addEventListener("submit", function(event) {
+    event.preventDefault();
+    if (!elementoFormulario.checkValidity()) {
+        alert("Por favor, preencha todos os campos da transação");
+        return;
+    }
+
+    const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
+    const inputValor = elementoFormulario.querySelector("#valor");
+    const inputData = elementoFormulario.querySelector("#data");
+
+    let TipoTransacao = inputTipoTransacao.value;
+    let valor = inputValor.value;
+    let data = inputData.value;
+
+    if (TipoTransacao == "Depósito") {
+        saldo += valor;
+    } else if (TipoTransacao == "Transferência" || TipoTransacao == "Pagamento de Boleto") {
+        saldo -= valor;
+    } else {
+        alert("Tipo de Transação é inválido!");
+        return;
+    }
+
+    elementoSaldo.textContent = saldo;
+
+    const novaTransacao = {
+        TipoTransacao: TipoTransacao,
+        valor: valor,
+        data: data
+    }
+
+    console.log(novaTransacao);
+    elementoFormulario.reset();
+});
