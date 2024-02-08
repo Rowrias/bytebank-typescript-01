61695ac6aff2382cb77e9994764fbd9e5f7e94d0
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2024-02-08 09:02:11 -0300

    4.1 Organizando os arquivos como módulos parte 01

--- dist/js/utils/formatters.js
+++ dist/js/utils/formatters.js
@@ -1,8 +1,8 @@
-function formatarMoeda(valor: number) : string {
+import { FormatoData } from "../types/FormatoData.js";
+export function formatarMoeda(valor) {
     return valor.toLocaleString("pt-br", { style: "currency", currency: "BRL" });
 }
-
-function formatarData(data: Date, formato: FormatoData = FormatoData.PADRAO) : string {
+export function formatarData(data, formato = FormatoData.PADRAO) {
     if (formato === FormatoData.DIA_SEMANA_DIA_MES_ANO) {
         return data.toLocaleDateString("pt-br", {
             weekday: "long",
@@ -12,8 +12,7 @@ function formatarData(data: Date, formato: FormatoData = FormatoData.PADRAO) : s
         });
     }
     else if (formato === FormatoData.DIA_MES) {
-        return data.toLocaleDateString("pt-br", {day: "2-digit", month: "2-digit"});
+        return data.toLocaleDateString("pt-br", { day: "2-digit", month: "2-digit" });
     }
-
     return data.toLocaleDateString("pt-br");
 }
