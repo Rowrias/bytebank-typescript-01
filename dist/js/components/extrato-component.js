dae388f01b86aedade6ac20462756fcc696b9f3b
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2024-02-09 16:02:24 -0300

    5.4 Componente de extrato

---
+++ dist/js/components/extrato-component.js
@@ -0,0 +1,34 @@
+import Conta from "../types/Contas.js";
+import { FormatoData } from "../types/FormatoData.js";
+import { formatarMoeda, formatarData } from "../utils/formatters.js";
+const elementoRegistroTransacoesExtrato = document.querySelector(".extrato .registro-transacoes");
+renderizarExtrato();
+function renderizarExtrato() {
+    const gruposTransacoes = Conta.getGrupoTransacoes();
+    elementoRegistroTransacoesExtrato.innerHTML = "";
+    let htmlRegistroTransacoes = "";
+    for (let GrupoTransacao of gruposTransacoes) {
+        let htmlTransacaoItem = "";
+        for (let transacao of GrupoTransacao.transacoes) {
+            htmlTransacaoItem += `
+                <div class="transacao-item">
+                    <div class="transacao-info">
+                        <span class="tipo">${transacao.tipoTransacao}</span>
+                        <strong class="valor">${formatarMoeda(transacao.valor)}</strong>
+                    </div>
+                    <time class="data">${formatarData(transacao.data, FormatoData.DIA_MES)}</time>
+                </div>
+            `;
+        }
+        htmlRegistroTransacoes += `
+            <div class="transacoes-group">
+                <strong class="mes-group">${GrupoTransacao.label}</strong>
+                ${htmlTransacaoItem}
+            </div>
+        `;
+    }
+    if (htmlRegistroTransacoes === "") {
+        htmlRegistroTransacoes = "<div>Não há transações registradas.</div>";
+    }
+    elementoRegistroTransacoesExtrato.innerHTML = htmlRegistroTransacoes;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
 import "./components/data-component.js"
 import "./components/nova-transacao-component.js"
 import "./components/saldo-component.js"
-import "./types/FormatoData.js"
-import "./types/TipoTransacao.js"
-import "./types/Transacao.js"
-import "./utils/formatters.js"
+import "./components/extrato-component.js"
